da571aa03f16d04ac0929bc2b3054aa2554fe435
Sondre <sonfjell@gmail.com>
2022-10-13 00:50:28 +0200

    Remove unnecessary nesting

--- src/components/PlaceTags.js
+++ src/components/PlaceTags.js
@@ -1,14 +1,12 @@
 function PlaceTags({ tags }) {
   return <> {tags.map((tag) => {
-    const styles = {
-      tagStyle: {
+    const tagStyle = {
         left: `${tag.location.left}%`, 
         top: `${tag.location.top}%`,
         width: `${tag.location.right - tag.location.left}%`,
         height: `${tag.location.bottom - tag.location.top}%`,
-      }
     }
-    return <div key={tag.id + 'f'} className={`tag ${tag.id}`} style={styles.tagStyle}></div>
+    return <div key={tag.id + 'f'} className={`tag ${tag.id}`} style={tagStyle}></div>
   })}</>
 }
 
